3a162676e00e120e9e6cc6f338f542fcd873b9a3
unknown <ayush.mimani@neosoftmail.com>
2026-08-28 12:33:19 +0530

    chnage in server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-  app.listen(PORT, "127.0.0.1", () => {
+  app.listen(PORT, "0.0.0.0", () => {
   console.log("Raw server running on 3000");
 });

3017e61e32342630b70a68eda0ccee3beecdaa23
unknown <ayush.mimani@neosoftmail.com>
2026-09-15 16:31:27 +0530

    check token mesage

--- Controller/userController.js
+++ Controller/userController.js
@@ -21,6 +21,7 @@ try{
         maxAge: 24 * 60 *60*1000,
       })
 
+      
       return res.status(200).json({
           status:true,
          message:"Login successfully",
--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async (req,res,next)=>{
     try {
          const {token}= req.cookies;
          if(!token){
-             throw new Error("Token is missing");
+             throw new Error("Token is missing890");
          }
 
          const decodedid = await jwt.verify(token,process.env.SECRET_JWT);
